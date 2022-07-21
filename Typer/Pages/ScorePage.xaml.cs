dcd6afc9ecb88372320db57e414e95188db2e60f
JonasKucinskas <jonaskucinskas2@gmail.com>
2022-07-21 21:07:09 +0300

    ,

--- Typer/Pages/ScorePage.xaml.cs
+++ Typer/Pages/ScorePage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -21,26 +23,13 @@ namespace Typer.Pages
     /// </summary>
     public partial class ScorePage : Page
     {
-
-        public class Score
-        {
-            public string Name { get; set; }
-            public int WordsTyped { get; set; }
-            public int Time { get; set; }
-        }
-
-
-
         public ScorePage()
         {
             InitializeComponent();
 
-
-            ScoreList.CanUserAddRows = true;
-
-            var list = new ObservableCollection<Score>();
-            list.Add(new Score() { Name = "testas", WordsTyped = 7, Time = 5 });
-            this.ScoreList.ItemsSource = list;
+            //this.ScoreTable.ItemsSource = users;
         }
+
+        
     }
 }
